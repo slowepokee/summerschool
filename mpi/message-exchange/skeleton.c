23b99bcf63e6503056bdde55b79c3204aafb6a23
Seppä Riikka E <riikka.seppa@helsinki.fi>
2022-06-27 11:21:53 +0300

    Skeleton.c

---
+++ mpi/message-exchange/skeleton.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<mpi.h>
+
+
+int main(int argc, char *argv[])
+{
+    int i, myid, ntasks, nrec;
+    int msgsize = 100;
+    int *message;
+    int *receiveBuffer;
+    MPI_Status status;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+    /* Allocate message */
+    message = (int *)malloc(sizeof(int) * msgsize);
+    receiveBuffer = (int *)malloc(sizeof(int) * msgsize);
+    /* Initialize message */
+    for (i = 0; i < msgsize; i++) {
+        message[i] = myid;
+    }
+
+    /* Send and receive messages as defined in exercise */
+    if (myid == 0) {
+        MPI_Send(message, msgsize, MPI_INT, 1, 1, MPI_COMM_WORLD);
+        MPI_Recv(receiveBuffer, msgsize, MPI_INT, 1, 2, MPI_COMM_WORLD, &status);
+
+        MPI_Get_count(&status, MPI_INT, &nrec);
+        printf("Rank %i received %i, num of el: %i\n", myid, receiveBuffer[0], nrec);
+    } else if (myid == 1) {
+        MPI_Send(message, msgsize, MPI_INT, 0, 2, MPI_COMM_WORLD);
+        MPI_Recv(receiveBuffer, msgsize, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+        
+
+        MPI_Get_count(&status, MPI_INT, &nrec);
+        printf("Rank %i received %i, num of el: %i\n", myid, receiveBuffer[0], nrec);
+    }
+
+    free(message);
+    free(receiveBuffer);
+    MPI_Finalize();
+    return 0;
+}
